c801700c8d2f59d12998e2cc6bc423c4747ef269
dtc292003 <duongtancuong292003@gmail.com>
2024-03-22 17:11:18 +0700

    web ban hang

---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+﻿use [D:\3.STUDY\KỲ 6\WEBSITEBANHANG\WEBSITEBANHANG\APP_DATA\QUANLYBANHANG.MDF]
+update SanPham
+set HinhAnh1 = 'iphone-6-plus.png',HinhAnh2 = 'iphone-6-plus.png',HinhAnh3 = 'iphone-6-plus.png',HinhAnh4 = 'iphone-6-plus.png'
+where MaSP = 16;
